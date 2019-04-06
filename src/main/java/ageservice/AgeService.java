6b19b32fa3b72990cfb95b3df184ff6303e5f3fa
driss rais <drissrais@gmail.com>
2019-04-06 14:23:12 +0100

    Générations de fichiers à l'aide de wsimport et implémentation de l'interface effectuées

--- src/main/java/ageservice/AgeService.java
+++ src/main/java/ageservice/AgeService.java
@@ -1,4 +1,13 @@
 package ageservice;
 
-public class AgeService {
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService(serviceName = "getAge")
+public class AgeService implements generated.ageservice.AgeService {
+    @Override
+    @WebMethod
+    public String getAge(Integer naissance) {
+        return "Votre âge est de " + (2019 - naissance) + " ans";
+    }
 }
---
+++ src/main/java/generated/ageservice/GetAgeResponse.java
@@ -0,0 +1,62 @@
+
+package generated.ageservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getAgeResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getAgeResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getAgeResponse", propOrder = {
+    "_return"
+})
+public class GetAgeResponse {
+
+    @XmlElement(name = "return")
+    protected String _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturn(String value) {
+        this._return = value;
+    }
+
+}
